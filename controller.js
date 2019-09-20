1cdc30682ab5316e612235efb1ede7c1a0ad0668
Dave Lea <davegt40@gmail.com>
2019-09-19 21:48:14 -0400

    restructure js and create reddit posts

--- controller.js
+++ controller.js
@@ -1,23 +1,4 @@
-/**
- * Get posts of a specific type from a subreddit.
- * 
- * @param {string} subreddit - name of subreddit
- * @param {string} type - type of posts (e.g., 'new', 'top', 'all')
- * @return {undefined}
- */
-function getPosts(subreddit, type) {
-  const url = 'https://www.reddit.com/r/' + subreddit + '/' + type + '.json';
+import * as helper from "./helper.js";
 
-  fetch(url)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (json) {
-      console.log(json);
-    })
-    .catch(function (error) {
-      console.log('There was an issue fetching the data. ' + response.error);
-    });
-}
-
-getPosts('webdev', 'new');
+// load reddit posts
+helper.getPosts('webdev', 'new');
