b2aed8737876d4db30601ed66facaf2816f40894
Dave Lea <davegt40@gmail.com>
2019-09-24 23:10:31 -0400

    play on load audio

--- controller.js
+++ controller.js
@@ -1,4 +1,7 @@
 import * as helper from "./helper.js";
 
+// play on load sounds
+let audio = document.querySelector('#good-morning-dave').play();
+
 // load reddit posts
 helper.getPosts('webdev', 'new');
