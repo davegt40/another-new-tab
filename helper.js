52f7a09102612aa48b8ed51d4a015fac6826559c
Dave Lea <davegt40@gmail.com>
2019-10-02 00:51:42 -0400

    make reddit cards

--- helper.js
+++ helper.js
@@ -34,17 +34,40 @@ function createPostItems(data) {
 
   if (postsList) {
     for (let item of data) {
-      let listItem = document.createElement('li');
-
-      let anchorItem = document.createElement('a');
-      anchorItem.setAttribute('href', item.data.url);
-      anchorItem.setAttribute('alt', item.data.title);
-      anchorItem.innerText = item.data.title;
-
-      listItem.appendChild(anchorItem);
-      postsList.appendChild(listItem);
+      postsList.appendChild(createPostElement(item));
     }
   } else {
     console.log('No data');
   }
+}
+
+/**
+ * Create a node element representing a reddit post
+ * 
+ * @param {object} item
+ * @return {node} html node representing a reddit post
+ */
+function createPostElement(item) {
+  let container = document.createElement('div');
+  container.setAttribute('class', 'c-reddit-post');
+
+  const link = document.createElement('a');
+  link.setAttribute('href', item.data.url);
+  link.setAttribute('alt', item.data.title);
+
+  const image = document.createElement('img');
+
+  // thumbnail image may not always be available
+  let imageUrl = (item.data.thumbnail === 'self') ? './images/code-solid.svg' : item.data.thumbnail;
+  image.setAttribute('src', imageUrl);
+  image.setAttribute('class', 'c-reddit-post__image')
+
+  const paragraph = document.createElement('p');
+  paragraph.innerText = item.data.title;
+
+  link.appendChild(image);
+  link.appendChild(paragraph);
+  container.appendChild(link);
+
+  return container;
 }
